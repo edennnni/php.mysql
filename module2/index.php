767ab3c76f1a0590c3ed4220bd6dc2ebaeebda59
edennnni <110981975+edennnni@users.noreply.github.com>
2025-01-23 16:17:11 +0100

    module2

---
+++ module2/index.php
@@ -0,0 +1,33 @@
+<?php 
+
+echo "Hello World";
+$emri="Edeni";
+$mosha= "16";
+$pesha="63kg";
+$is_true = true;
+
+
+
+echo $emri;
+echo $mosha;
+echo $pesha;
+echo $is_true;
+
+?> 
+
+<?php
+
+$str = "Hello fri3nd, you're
+       looking          good today!";
+
+echo str_word_count($str);
+
+
+$str = 'eedd';
+echo strlen($str); 
+
+$str = ' ed en ';
+echo strlen($str); 
+?>
+
+?>

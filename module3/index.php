a5bf8761f0e64f747f121fc6b9660acadf1440e9
edennnni <110981975+edennnni@users.noreply.github.com>
2025-01-28 16:19:54 +0100

    php

---
+++ module3/index.php
@@ -0,0 +1,39 @@
+<?php 
+
+$grade = 80;
+
+if ($grade <= 90); {
+    echo("you got an 5");
+}
+elseif ($grade <= 80 && $grade 90){
+    echo("you got an 4")
+}
+elseif ($grade <= 60 && $grade 70)
+    echo("you got an 3")
+elseif ($grade <= 50 && $grade )
+?> 
+
+<?php
+
+
+$day = "Monday"
+switch($day){
+case "Monday":
+    echo "Nuk ki kurs";
+    break;
+case "Tuesday":
+    echo "ki kurs";
+    break;
+case "Wednesday":
+    echo "Nuk ki kurs";
+    break;
+case "Thursday":
+    echo "ki kurs";
+    break;
+case "Friday":
+    echo "Nuk ki kurs";
+    break;
+default:
+    echo "Nuk ki kurs";
+}
+?>

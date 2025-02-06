fc288a03552591964c5d9b11c5f112f11af6cb59
Eden <110981975+edennnni@users.noreply.github.com>
2025-02-06 16:18:10 +0100

    m4 3

--- module4/index.php
+++ module4/index.php
@@ -1,27 +1,67 @@
 <?php 
 
-function sum ($n1, $n2 ) {
+function sum ($nr1, $nr2 ) {
 
     if ($nr1 > $nr2){
         return $nr1;
     }
     else {
-        return $n2;
+        return $nr2;
     }
 }
 
-echo find_max(15,30);
-echo("<br>")
+echo sum(15,30);
+echo("<br>");
 function qift($x){
     if($x%2==0){
-echo "$x eshte qift"
+echo "$x eshte qift";
     }
 else{
-    echo "$x nuk eshte qift"
+    echo "$x nuk eshte qift";
     }
 }
 
 $numri = qift (5); 
-echo ($numri)
+echo ($numri);
+
+
+$students =["Edeni","Nil","Erjoni","Anik"];
+
+array_push($students,"Edeni");
+
+print_r($students);
+
+$count=count($students);
+
+for($i=1;$i<$count;$i++){
+    echo $students [$i]. "<br>";
+}
+
+array_pop($students);
+
+print_r($students);
+
+
+array_unshift($students,"Anik");
+
+echo "<br>";
+
+print_r($students);
+
+array_shift($students);
+
+echo "<br>";
+
+$students_slice=array_slice($students, 1,1);
+print_r ($students_slice);
+
+echo "<br>";
+
+$mosha=[16,14,13,15,16,14,15];
+$shuma=array_sum($mosha);
+
+$count= count($mosha);
+
+print_r($shuma / $count );
 
 ?>

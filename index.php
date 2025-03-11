7863991befddfb771da85e50861894944d3d639c
Eden <110981975+edennnni@users.noreply.github.com>
2025-03-11 16:17:20 +0100

    mod10

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <form action="add.php" method= "POST">    
 
 <label type="text" id="name" name="name" placeholder="Name"><br>
-<input type="text" id="surname" name="username" placeholder="surname"><br>
+<input type="text" id="surname" name="surname" placeholder="surname"><br>
 <label type="email" id="email" name="email" placeholder="Email"><br>
 <button type="submit" name="submit">Add</button>
 </form>

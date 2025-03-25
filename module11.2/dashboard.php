49a5135d434e7e1f171b25218a1a76c210cbf219
Eden <110981975+edennnni@users.noreply.github.com>
2025-03-25 08:14:16 -0700

    m12

--- module11.2/dashboard.php
+++ module11.2/dashboard.php
@@ -3,7 +3,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lesson 11</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <style>
     table,td,th {
         border: 1px solid black;
@@ -42,7 +47,8 @@
             <td> <?= $user['email']?></td>
             <td> <?= $user['age']?></td>
             <td> <?="<a href='edit.php?id=$user[id]'>Edit</a>"?>
-            | <?="<a href='delete.php?id=$user[id]'> Delete </a>"?></td>
+            | <?="<a href='delete.php?id=$user[id]'> Delete </a> :
+             <a href='edit.php?id=$User[id]'>Update </a>"?></td>
     </tr>
     <?php  
     }  
@@ -50,5 +56,9 @@
 </body>
 </table>
 <a href="index.php">Add User</a>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
+integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" 
+integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
--- module11.2/delete.php
+++ module11.2/delete.php
@@ -4,6 +4,8 @@ $username = "root";
 $password = ""; 
 $dbname = "db"; 
 
+$id = 1;
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {

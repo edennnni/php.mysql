2be9187547894cbb907e4b9a5096f36083b50b50
Eden <110981975+edennnni@users.noreply.github.com>
2025-04-01 07:07:03 -0700

    m13,2

--- module13/loginLogic.php
+++ module13/loginLogic.php
@@ -12,5 +12,23 @@ if(isset($_POST['submit'])){
   $sql="SELECT * FORM users WHERE username=:username";
   $insertSql=$conn->prepare($sql);
   $bindSql->bindParam(':username',$username);
-  $insertSql->execute();}
-?>
+  $insertSql->execute();} 
+  
+if($insertSql->rowcount() > 0 ) {
+  $data = $insertSql->fetch();
+  if(password_verify($password, $data['password'])) {
+    $_SESSION ['username']=$datap['username'];
+    header("Location: dashboard.php");
+  }
+}else{
+echo "password incorrect"
+header ("refresh:2; url=login.php");
+}else{
+  echo "username not found"
+}
+
+  
+  
+  
+  ?>
+

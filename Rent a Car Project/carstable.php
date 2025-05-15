605b45fd976bb813d623273f8fb88389eb6aeb23
Eden <110981975+edennnni@users.noreply.github.com>
2025-05-15 07:13:07 -0700

    wersdff

--- Rent a Car Project/carstable.php
+++
@@ -1,8 +0,0 @@
-<?  TABLE cars 
-    car_id INT AUTO_INCREMENT PRIMARY KEY,
-    model VARCHAR(100),
-    price_per_day DECIMAL(10,2),
-    features TEXT,
-    image VARCHAR(255),
-    availability BOOLEAN DEFAULT 1  -- 1 means available, \
-?>
